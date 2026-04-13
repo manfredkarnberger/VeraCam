e104133287292de7375b01b3573d26a3f29fcfc2
Manfred Karnberger <karnberger.manfred@gmail.com>
2026-04-13 15:07:00 +0200

    minor

--- obj/x64/Debug/net8.0-windows/VeraCam.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows/VeraCam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeraCam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e30ea66c7babac4532283154181de7fbe65897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e63b935a51e288e85bf56caef008dca7a92d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeraCam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeraCam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
